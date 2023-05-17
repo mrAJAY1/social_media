5ad44dc594684d81ffca24f29bc65d20f40a8a07
mrAJAY1 <thisismeajayprakash@gmail.com>
2023-05-18 03:59:24 +0530

    changed postSignup req.body to object destructuring

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,14 +1,14 @@
 import { userSignup } from '../helpers/media_helper.js';
 
 function postSignup(req, res) {
-  const { body } = req;
+  const { body:{name,phone,email,username,password,dob,profile_img} } = req;
   const result = userSignup({
-    name:body.name,
-    phone:body.phone,
-    email:body.email,
-    user_name:body.username,
-    password:body.password,
-    dob:body.dob,
-    profile_img:body.profile_img
+    name:name,
+    phone:phone,
+    email:email,
+    user_name:username,
+    password:password,
+    dob:dob,
+    profile_img:profile_img
   })
 }
